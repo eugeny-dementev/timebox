8e0f2ad573fd6f821444447ac1b449f2a286c99b
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-03-27 17:09:19 +0800

    fixes

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -3,7 +3,7 @@ import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import App from './App.tsx';
 
-export function SSRender(url: string | Partial<Location>) {
+export default function SSRender(url: string | Partial<Location>) {
   return ReactDOMServer.renderToString(
     <StaticRouter location={url}>
       <App />
