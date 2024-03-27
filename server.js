72b740669b8bf53f1ee08dc504f85a9d1cc4c123
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-03-27 17:09:19 +0800

    fixes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ export async function createServer() {
     const url = '/';
 
     const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8');
-    const render = (await import('./dist/server/entry-server.js')).SSRRender;
+    const render = (await import('./dist/server/entry-server.js')).default;
 
     const appHtml = render(url); //Rendering component without any client side logic de-hydrated like a dry sponge
     const html = template.replace(`<!--app-html-->`, appHtml); //Replacing placeholder with SSR rendered components
