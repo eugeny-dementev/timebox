a80904c6aecef5355241f1cfbe14357f45efff04
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-03-28 15:06:33 +0800

    fix: tailwindcss configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     minify: false,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
 });
