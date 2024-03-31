7b5db0a8b88f4504e3b54923664c4354ca2c66bf
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-03-31 23:21:21 +0800

    embed old_generate.html as iframe to react app

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import css from 'style-to-object';
 
 export default function Generate() {
   return (
-    <div>Generate</div>
+    <div>
+      <iframe
+        style={css('display: block; position: absolute; width: 100%; height: 100%; border: none; overflow: hidden;') as CSSProperties}
+        src='/old_generate.html'
+        sandbox="allow-scripts allow-same-origin allow-downloads"
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
+        scrolling="no"
+      >
+      </iframe>
+    </div>
   )
 }
