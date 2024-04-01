16157b1f8c74af18ba6061db4c159027130320a5
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-04-01 15:44:21 +0800

    rework ssg renderer

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import { basename } from '../options.js';
 
 ReactDOM.hydrateRoot(
   document.getElementById('app'),
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
