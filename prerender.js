4f45fe8315dd67310ba258fac5036bd45774f470
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-04-01 15:44:21 +0800

    rework ssg renderer

--- prerender.js
+++ prerender.js
@@ -1,27 +1,28 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
+import { basename } from './options.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const toAbsolute = (p) => path.resolve(__dirname, p);
 
 const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8');
-const render = (await import('./dist/server/entry-server.js')).default;
-
-// determine routes to pre-render from src/pages
-const routesToPrerender = fs.readdirSync(toAbsolute('src/pages')).map((file) => {
-  const name = file.replace(/\.tsx$/, '').toLowerCase();
-  return name === 'home' ? `/` : `/${name}`;
-});
 
 (async () => {
+  // determine routes to pre-render from src/pages
+  const routesToPrerender = fs
+    .readdirSync(toAbsolute('src/pages'))
+    .map((file) => file.replace(/\.tsx$/, '').toLowerCase())
+
+  const render = (await import('./dist/server/entry-server.js')).default;
   // pre-render each route...
   for (const url of routesToPrerender) {
-    const appHtml = render(url);
+    const fullUrl = path.resolve('/', basename, url);
+    const appHtml = render(fullUrl);
 
     const html = template.replace(`<!--app-html-->`, appHtml);
 
-    const filePath = `dist/static${url === '/' ? '/index' : url}.html`;
+    const filePath = path.resolve('dist', 'static', `${url === 'about' ? 'index' : url}.html`);
     fs.writeFileSync(toAbsolute(filePath), html);
   }
 })();
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import App from './App.tsx';
+import { basename } from '../options.js';
 
 export default function SSRender(url: string | Partial<Location>) {
   return ReactDOMServer.renderToString(
     <React.StrictMode>
-      <StaticRouter location={url}>
+      <StaticRouter location={url} basename={basename}>
         <App />
       </StaticRouter>
     </React.StrictMode>
