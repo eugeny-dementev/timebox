bc9d85b41a30a72f6169738e8c5f005557221278
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-04-05 19:34:35 +0800

    improve dev mode

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
+const isProduction = process.env.NODE_ENV === "production";
+
+const profiling = isProduction && {
+  "react-dom/client": "react-dom/profiling",
+};
 
 export default defineConfig({
   plugins: [react()],
   base: '/timebox',
+  mode: 'dev',
   build: {
     minify: false,
   },
   css: {
     postcss: {
       plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    alias: {
+      ...profiling,
     },
   },
 });
