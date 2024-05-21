61e2de361762fce78f1d096e3eddb599b42c4237
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-21 22:41:15 +0800

    feat: extract current daily doc into version 1

--- daily.mjs
+++ daily.mjs
@@ -8,9 +8,13 @@ import { drawDailyPlanner } from "./drawDailyPlanner.mjs";
 export function getDailyDoc({
   cellSize = 9.5,
   startingHour = 8,
+  version = 1,
 }) {
-  const doc = prepareDoc();
+  if (version === 1) {
+    return getDailyDocThicc({ cellSize, startingHour });
+  }
 
+  const doc = prepareDoc();
 
   // Top priorities
   const tpBottomY = drawTopSection(doc, startX, startY, sectionW, cellSize)
@@ -35,6 +39,38 @@ export function getDailyDoc({
   doc.text(124, 21, ":30");
 
   // Draw daily planning section with 30 mintues split for 19 hours of the day starting with 08:00
+  drawDailyPlanner(doc, pSectionX, pSectionY, sectionW, pSectionH, 8, 19);
+
+  return doc;
+}
+
+function getDailyDocThicc({ cellSize = 9.5, startingHour = 8 }) {
+  const doc = prepareDoc();
+
+
+  // Top priorities
+  const tpBottomY = drawTopSection(doc, startX, startY, sectionW, cellSize)
+
+
+  // Brain dump
+  const bdY = tpBottomY + cellSize - approximateLabelH;
+  const bdH = endY - tpBottomY;
+  drawBrainDump(doc, startX, bdY, sectionW, bdH);
+  drawDots(doc, startX, bdY + approximateLabelH, sectionW, bdH, cellSize);
+
+  const pSectionX = startX + sectionW + cellSize;
+  doc.text(pSectionX, startY, "Date:");
+  const pLabelY = startY + approximateLabelH;
+  const pLabelX = pSectionX + 10.5;
+  doc.line(pLabelX, pLabelY, endX, pLabelY);
+  const pSectionY = pLabelY + cellSize;
+  const pSectionH = endY - pSectionY + cellSize;
+
+  // Half hour labels with approximatly coordinates
+  doc.text(100, 21, ":00");
+  doc.text(124, 21, ":30");
+
+  // Draw daily planning section with 30 minutes split for 19 hours of the day starting with 08:00
   drawDailyPlanner(doc, pSectionX, pSectionY, sectionW, pSectionH, 8, 19);
 
   return doc;
