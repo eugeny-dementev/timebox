a556efe0c35c9d4ccaf1fcb0ee49b4ec4b84be24
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-21 23:18:27 +0800

    feat: implement daily planner doc v2

--- daily.mjs
+++ daily.mjs
@@ -3,7 +3,7 @@ import { drawTopSection } from "./drawTopSection.mjs";
 import { drawBrainDump } from "./drawBrainDump.mjs";
 import { drawDots } from "./drawDots.mjs";
 import { startY, startX, sectionW, endY, endX, approximateLabelH } from "./constants.mjs";
-import { drawDailyPlanner } from "./drawDailyPlanner.mjs";
+import { drawDailyPlanner, drawDailyPlanner2 } from "./drawDailyPlanner.mjs";
 
 export function getDailyDoc({
   cellSize = 9.5,
@@ -17,29 +17,23 @@ export function getDailyDoc({
   const doc = prepareDoc();
 
   // Top priorities
-  const tpBottomY = drawTopSection(doc, startX, startY, sectionW, cellSize)
-
+  const tpBottomY = drawTopSection(doc, startX, startY + cellSize, sectionW, cellSize)
 
   // Brain dump
   const bdY = tpBottomY + cellSize - approximateLabelH;
   const bdH = endY - tpBottomY;
   drawBrainDump(doc, startX, bdY, sectionW, bdH);
-  drawDots(doc, startX, bdY + approximateLabelH, sectionW, bdH, cellSize);
+  drawDots(doc, startX, bdY + approximateLabelH, sectionW, bdH - cellSize, cellSize);
 
   const pSectionX = startX + sectionW + cellSize;
-  doc.text(pSectionX, startY, "Date:");
+  doc.text(pSectionX - sectionW - cellSize, startY, "Date:");
   const pLabelY = startY + approximateLabelH;
-  const pLabelX = pSectionX + 10.5;
-  doc.line(pLabelX, pLabelY, endX, pLabelY);
+  doc.line(startX + 10.5, pLabelY, startX + sectionW, pLabelY);
   const pSectionY = pLabelY + cellSize;
   const pSectionH = endY - pSectionY + cellSize;
 
-  // Half hour labels with approxmimate coordinates
-  doc.text(100, 21, ":00");
-  doc.text(124, 21, ":30");
-
-  // Draw daily planning section with 30 mintues split for 19 hours of the day starting with 08:00
-  drawDailyPlanner(doc, pSectionX, pSectionY, sectionW, pSectionH, 8, 19);
+  // Draw daily planning section with 30 minutes split for 19 hours of the day starting with 08:00
+  drawDailyPlanner2(doc, pSectionX, pLabelY, sectionW, pSectionH + cellSize, 9, 17);
 
   return doc;
 }
@@ -47,10 +41,8 @@ export function getDailyDoc({
 function getDailyDocThicc({ cellSize = 9.5, startingHour = 8 }) {
   const doc = prepareDoc();
 
-
   // Top priorities
   const tpBottomY = drawTopSection(doc, startX, startY, sectionW, cellSize)
-
 
   // Brain dump
   const bdY = tpBottomY + cellSize - approximateLabelH;
@@ -66,7 +58,7 @@ function getDailyDocThicc({ cellSize = 9.5, startingHour = 8 }) {
   const pSectionY = pLabelY + cellSize;
   const pSectionH = endY - pSectionY + cellSize;
 
-  // Half hour labels with approximatly coordinates
+  // Half hour labels with approximate coordinates
   doc.text(100, 21, ":00");
   doc.text(124, 21, ":30");
 
--- drawDailyPlanner.mjs
+++ drawDailyPlanner.mjs
@@ -35,3 +35,36 @@ export function drawDailyPlanner(doc, x, y, w, h, startingHour = 8, hours = 19) 
     drawDots(doc, x + cellSize + halfHourW, rowY, halfHourW, cellSize, cellSize);
   }
 }
+
+export function drawDailyPlanner2(doc, x, y, w, h, startingHour = 8, hours = 19) {
+  doc.rect(x, y, w, h);
+
+  const cellSize = h / hours;
+
+  // draw separation line for hour hours labels 
+  // with the same width as row height (cell size)
+  const hoursX = x + cellSize
+  doc.line(hoursX, y, hoursX, y + h);
+
+  // draw  separation line between half hour rows
+  const hourW = w - cellSize;
+
+  const hourLabelX = x + cellSize / 2 - 2; // approxmimate coordinate for hour label X coordinate
+  const hourLabelYShift = cellSize / 2 + 1; // approxmimate shift for hour label Y coordinate
+
+  const getHourLabel = prepareHoursIterator(startingHour);
+
+  for (let r = 0; r < hours; r++) {
+    const hourLabelY = y + cellSize * r + hourLabelYShift;
+    doc.text(hourLabelX, hourLabelY, getHourLabel());
+
+    const rowY = y + cellSize * r;
+
+    if (r != 0) {
+      doc.line(x, rowY, hoursX, rowY)
+    }
+
+    if (r !== hours - 1) drawDots(doc, x + cellSize, rowY + cellSize - cellSize/4, hourW, cellSize / 2, cellSize / 2);
+    drawDots(doc, x + cellSize, rowY, hourW, cellSize, cellSize);
+  }
+}
